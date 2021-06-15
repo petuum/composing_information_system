e552313d030e1fdba49ee2b96531f3a6b10e4cef
jingzhi.pang@petuum.com <jingzhi.pang@petuum.com>
2021-06-15 15:29:53 -0700

    add testcase for indexer pipeline

--- tests/examples/indexer_pipeline_test.py
+++ tests/examples/indexer_pipeline_test.py
@@ -1,1 +1,12 @@
+import unittest
 from examples.pipeline.indexer.cordindexer import *
+
+
+class BuildIndexPipelineTest(unittest.TestCase):
+    def test_pipeline(self):
+        config_file = 'sample_data/tests/config.yml'
+        config = yaml.safe_load(open(config_file, "r"))
+        config = Config(config, default_hparams=None)
+
+        data_dir = 'sample_data/tests/cord19research'
+        build_index_pipeline(data_dir, config)
